356c03cb309ba5da3c0a2929f1dc76f3a4fb5659
June <juneh017@gmail.com>
2016-04-09 16:04:09 -0400

    bitcamp initial commit april 9

---
+++ app/src/androidTest/java/joon21/bitcamp_prototype00/ApplicationTest.java
@@ -0,0 +1,13 @@
+package joon21.bitcamp_prototype00;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
